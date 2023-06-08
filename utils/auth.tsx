9259e2a682f6b76e852858c8d2b0b6f1be65baee
Hunter Barton <axiysfire@gmail.com>
2023-06-08 01:21:21 -0500

    added api url

--- utils/auth.tsx
+++ utils/auth.tsx
@@ -1,81 +1,97 @@
-import React, { useState, useContext, createContext, FC, useEffect } from 'react';
-import { ApolloProvider, ApolloClient, InMemoryCache, HttpLink, NormalizedCacheObject } from '@apollo/client';
+import React, {
+  useState,
+  useContext,
+  createContext,
+  FC,
+  useEffect
+} from 'react'
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  NormalizedCacheObject
+} from '@apollo/client'
 
 type AuthContextType = {
-   setAuthToken: React.Dispatch<React.SetStateAction<string | null>>;
-   isSignedIn: () => boolean;
-   createApolloClient: () => ApolloClient<NormalizedCacheObject>;
-   signIn: (token: string) => void;
-   signOut: () => void;
-};
+  setAuthToken: React.Dispatch<React.SetStateAction<string | null>>
+  isSignedIn: () => boolean
+  createApolloClient: () => ApolloClient<NormalizedCacheObject>
+  signIn: (token: string) => void
+  signOut: () => void
+}
 
-const AuthContext = createContext(null as any as AuthContextType);
+const AuthContext = createContext(null as any as AuthContextType)
 
 export const AuthProvider: FC = ({ children }) => {
-   const auth = useProvideAuth();
+  const auth = useProvideAuth()
 
-   useEffect(() => {
-      localStorage.getItem('auth-token') ? auth.setAuthToken(localStorage.getItem('auth-token')) : null;
-   }, []);
+  useEffect(() => {
+    localStorage.getItem('auth-token')
+      ? auth.setAuthToken(localStorage.getItem('auth-token'))
+      : null
+  }, [])
 
-   return (
-      <AuthContext.Provider value={auth}>
-         <ApolloProvider client={auth.createApolloClient()}>{children}</ApolloProvider>
-      </AuthContext.Provider>
-   );
-};
+  return (
+    <AuthContext.Provider value={auth}>
+      <ApolloProvider client={auth.createApolloClient()}>
+        {children}
+      </ApolloProvider>
+    </AuthContext.Provider>
+  )
+}
 
 export const useAuth = () => {
-   return useContext(AuthContext);
-};
+  return useContext(AuthContext)
+}
 
 function useProvideAuth(): AuthContextType {
-   const [authToken, setAuthToken] = useState(null as null | string);
+  const [authToken, setAuthToken] = useState(null as null | string)
 
-   const signIn = (token: string) => {
-      setAuthToken(token);
-      localStorage.setItem('auth-token', token);
-   };
+  const signIn = (token: string) => {
+    setAuthToken(token)
+    localStorage.setItem('auth-token', token)
+  }
 
-   const signOut = () => {
-      setAuthToken(null);
-      localStorage.clear();
-   };
+  const signOut = () => {
+    setAuthToken(null)
+    localStorage.clear()
+  }
 
-   const isSignedIn = () => {
-      if (authToken) {
-         return true;
-      } else {
-         return false;
+  const isSignedIn = () => {
+    if (authToken) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  const getAuthHeaders = () => {
+    if (!authToken) return ``
+
+    return `Bearer ${authToken}`
+  }
+
+  const createApolloClient = () => {
+    const authorization = getAuthHeaders()
+    const link = new HttpLink({
+      uri: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001'}/gql`,
+      headers: {
+        authorization
       }
-   };
+    })
 
-   const getAuthHeaders = () => {
-      if (!authToken) return null;
+    return new ApolloClient({
+      link,
+      cache: new InMemoryCache()
+    })
+  }
 
-      return `Bearer ${authToken}`;
-   };
-
-   const createApolloClient = () => {
-      const authorization = getAuthHeaders();
-      const link = new HttpLink({
-         uri: 'https://hb-notes-backend.herokuapp.com/graphql',
-         headers: {
-            authorization
-         }
-      });
-
-      return new ApolloClient({
-         link,
-         cache: new InMemoryCache()
-      });
-   };
-
-   return {
-      setAuthToken,
-      isSignedIn,
-      createApolloClient,
-      signIn,
-      signOut
-   };
+  return {
+    setAuthToken,
+    isSignedIn,
+    createApolloClient,
+    signIn,
+    signOut
+  }
 }
